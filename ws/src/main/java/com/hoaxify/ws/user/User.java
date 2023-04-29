3bb57f3303adb9b326dd97dc0de14b4822bd9b3d
“Aqshin13” <aqsin.quliyev.17@inbox.ru>
2023-04-29 20:33:30 +0400

    First Commit for Backend

---
+++ ws/src/main/java/com/hoaxify/ws/user/User.java
@@ -0,0 +1,18 @@
+package com.hoaxify.ws.user;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private String username;
+    private String displayName;
+    private String password;
+
+
+}
