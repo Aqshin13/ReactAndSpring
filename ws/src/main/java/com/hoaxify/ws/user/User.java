fadb676b3be8e65c4064773557c47d474cd4bba0
“Aqshin13” <aqsin.quliyev.17@inbox.ru>
2023-05-01 22:45:37 +0400

    Spring Data

--- ws/src/main/java/com/hoaxify/ws/user/User.java
+++ ws/src/main/java/com/hoaxify/ws/user/User.java
@@ -1,6 +1,7 @@
 package com.hoaxify.ws.user;
 
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +9,13 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String username;
     private String displayName;
     private String password;
--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,12 +1,12 @@
 package com.hoaxify.ws.user;
 
 
+import com.hoaxify.ws.shared.GenericResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -14,11 +14,14 @@ public class UserController {
 
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
+    @Autowired
+    private UserService  userService;
 
     @PostMapping("/api/1.0/users")
-    public void createUser(@RequestBody User user) {
-
-        log.info(user.toString());
+//    @ResponseStatus(HttpStatus.CREATED)
+    public GenericResponse createUser(@RequestBody User user) {
+        userService.save(user);
+        return new GenericResponse("user created");
     }
 
 
